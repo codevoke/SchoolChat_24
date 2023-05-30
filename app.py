f827b7e96eaac031e56e115bfd0e39c5d60637b0
exp-of-betrayal <none@null.0>
2023-05-30 19:53:03 +0400

    add basic structure of site pages

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+from flask import Flask, send_file
+from flask_jwt_extended import JWTManager, jwt_required
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+
+jwt = JWTManager(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main_page():  # put application's code here
+    return send_file("templates/main.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/login')
+def login_page():
+    return send_file("templates/login.html")
+
+
+@app.route('/register')
+def register_page():
+    return send_file("templates/reg.html")
+
+
+@app.route("/me")
+@jwt_required()
+def msg_page():
+    return send_file("templates/chat.html")
+
+
+@jwt.unauthorized_loader
+def unauthorized_page(_):
+    return send_file("templates/401.html")
+
+
+app.run()
